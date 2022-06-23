3a4cd26dc04171e206b87da001a5bfeaded4a861
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-06-23 21:17:01 +0200

    Create books resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "books#index"
-  get "/books", to: "books#index"
-  get "/books/:id", to: "books#show"
+  Rails.application.routes.draw do
+    root "books#index"
+    resources :books
+  end
 end
