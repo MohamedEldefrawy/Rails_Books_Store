a19887a1001f63115fdd376cb167e609401a4820
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-06-23 22:15:55 +0200

    Add basic validation book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  validates :name, presence: true
+  validates :describtion, presence: true
+  validates :created_at, presence: false
+  validates :updated_at, presence: false
 end
