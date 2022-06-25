ec704b847f952b23a0804f715a71b8d568af8188
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-06-25 16:52:15 +0200

    Secure routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_25_132928) do
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_06_25_141244) do
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.text "photo"
@@ -40,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_25_132928) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
